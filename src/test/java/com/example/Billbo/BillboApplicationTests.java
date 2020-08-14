dfbdf1baa292ab44638c02f138297b1b84796077
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-13 23:22:26 -0400

    init billbo api

---
+++ src/test/java/com/example/Billbo/BillboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Billbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
